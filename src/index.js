5fa24c0fe08a8040fa3553c66e5fb0abcf2734e3
Ventiphresh <84814146+Ventiphresh@users.noreply.github.com>
2021-09-25 18:19:32 -0400

    inline Style

--- src/index.js
+++ src/index.js
@@ -10,21 +10,15 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+const customStyle = {
+  color: "red",
+  border: "10px 20px",
+  fontSize: "20px"
+};
+
 ReactDom.render(
   <div>
-    <h1 className="heading">Hello</h1>
-    <img
-      className="food-image"
-      src="https://www.rd.com/wp-content/uploads/2021/04/GettyImages-476744133-scaled.jpg?resize=768,511"
-    ></img>
-    <img
-      className="food-image"
-      src="https://www.investopedia.com/thmb/lqOcGlE7PI6vLMzhn5EDdO0HvYk=/1337x1003/smart/filters:no_upscale()/GettyImages-1054017850-7ef42af7b8044d7a86cfb2bff8641e1d.jpg"
-    ></img>
-    <img
-      className="food-image"
-      src="https://images.unsplash.com/photo-1587049352851-8d4e89133924?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aG9uZXl8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
-    ></img>
+    <h1 style={customStyle}>Hello</h1>
   </div>,
   document.getElementById("root")
 );
